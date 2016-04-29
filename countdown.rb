a243e84230608a10a17583fb09d16277f45405e1
hailuhr <hredleaf@gmail.com>
2016-04-29 00:58:51 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  countdown = int.to_i
+  until countdown == 0
+    puts "#{countdown} SECOND(S)!"
+    countdown -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(int)
+ countdown = int.to_i
+  until countdown == 0
+    puts "#{countdown} SECOND(S)!"
+    sleep(1)
+    countdown -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+
